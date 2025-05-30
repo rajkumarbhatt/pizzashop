8165fcda793507344c0c49279f0250794c416418
Rajkumar Bhatt <tatvasoft.pci88@outlook.com>
2025-05-30 18:54:55 +0530

    adding stored procedures in order app

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59d41854e35f1b43912e3be7d9b9cb87fedf7c0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b80392750f1b19e8dfc2c6ec44e077500367c6af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
