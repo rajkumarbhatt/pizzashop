be76696aae2269e78a35cf0b3a0d0b6bbd67bb28
Rajkumar Bhatt <mr.bhatt2348@gmail.com>
2026-03-18 07:38:53 +0530

    Enable 2fa hotfix

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8165fcda793507344c0c49279f0250794c416418")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0ef81f622e1c4aa0221a640a2a2d3fc2e39313f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
